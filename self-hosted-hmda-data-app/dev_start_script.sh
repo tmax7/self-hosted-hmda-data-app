565c5e0dd65d7c4d679ed5c392e625c30ab81475
Taylor Maxfield <taylor.p.maxfield@gmail.com>
2021-06-17 18:44:21 -0700

    Change location of log files to be in /var/log

--- self-hosted-hmda-data-app/dev_start_script.sh
+++ self-hosted-hmda-data-app/dev_start_script.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # Creates log files
-mkdir /logs
-touch /logs/celery_hmda_data_app.log
-touch /logs/gunicorn_hmda_data_app.log
+hmda_data_app_log_dir='/var/log/self-hosted-hmda-data-app/logs'
+touch "$hmda_data_app_log_dir/celery_hmda_data_app.log"
+touch "$hmda_data_app_log_dir/gunicorn_hmda_data_app.log"
 # Runs celery and gunicorn
-celery --app hmda_data_app.tasks worker --detach --logfile=/logs/celery_hmda_data_app.log --loglevel=WARNING
-gunicorn --config /self-hosted-hmda-data-app/gunicorn.conf.py
+celery --app hmda_data_app.tasks worker --detach --logfile="$hmda_data_app_log_dir/celery_hmda_data_app.log" --loglevel=WARNING
+gunicorn --config /self-hosted-hmda-data-app/gunicorn.conf.py --log-file="$hmda_data_app_log_dir/gunicorn_hmda_data_app.log" --log-level="warning"
